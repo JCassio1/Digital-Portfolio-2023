7b7251a4ee7a8972fee81f4799216cd986a8ae95
JCassio1 <Joselsondias@hotmail.com>
2023-05-24 23:39:29 +0100

    Add project section

--- JD Digital Portfolio 2023/src/Pages/Homepage/Homepage.tsx
+++ JD Digital Portfolio 2023/src/Pages/Homepage/Homepage.tsx
@@ -1,11 +1,13 @@
 import NavBar from '../../Components/Layout/NavBar'
 import Hero from './Components/Hero'
+import ProjectsSection from './Components/ProjectsSection'
 
 const Homepage = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <ProjectsSection />
     </>
   )
 }
