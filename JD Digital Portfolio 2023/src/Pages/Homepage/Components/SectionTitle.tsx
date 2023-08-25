c47bbc4f9c154d712c9f296cb2293711af049685
JCassio1 <Joselsondias@hotmail.com>
2023-08-25 16:01:47 +0300

    Add a description section

---
+++ JD Digital Portfolio 2023/src/Pages/Homepage/Components/SectionTitle.tsx
@@ -0,0 +1,13 @@
+const SectionTitle = () => {
+  return (
+    <section>
+      <div className='my-20 mx-auto max-w-screen-xl px-4'>
+        <p className='text-3xl font-bold'>
+          Transforming concepts into code, delivering projects from conception to completion, including{' '}
+          <span className='text-indigo-600'>requirements gathering, design, coding, testing, and iteration</span>.
+        </p>
+      </div>
+    </section>
+  )
+}
+export default SectionTitle
--- JD Digital Portfolio 2023/src/Pages/Homepage/Homepage.tsx
+++ JD Digital Portfolio 2023/src/Pages/Homepage/Homepage.tsx
@@ -2,12 +2,14 @@ import NavBar from '../../Components/Layout/NavBar'
 import Contacts from './Components/Contacts'
 import Hero from './Components/Hero'
 import ProjectsSection from './Components/ProjectsSection'
+import SectionTitle from './Components/SectionTitle'
 
 const Homepage = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <SectionTitle />
       <ProjectsSection />
       <Contacts />
     </>
