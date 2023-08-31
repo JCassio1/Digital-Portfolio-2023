219438239fd863022672bfcf3582c72f54c22751
JCassio1 <Joselsondias@hotmail.com>
2023-08-31 21:01:33 +0100

    Remove dark mode

--- JD Digital Portfolio 2023/src/Pages/Homepage/Components/ReviewCards.tsx
+++ JD Digital Portfolio 2023/src/Pages/Homepage/Components/ReviewCards.tsx
@@ -2,9 +2,9 @@ import { reviewCard } from '../../../model/DataStructure'
 
 const ReviewCards = ({ reviewComments, reviewBy, workType }: reviewCard) => {
   return (
-    <article className='my-8 mr-2 hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s] dark:shadow-gray-700/25'>
-      <div className='rounded-[10px] bg-white p-4  dark:bg-gray-900 sm:p-6'>
-        <h3 className='mt-0.5 text-lg font-medium text-gray-900 dark:text-white'>{reviewBy}</h3>
+    <article className='my-8 mr-2 hover:animate-background rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]'>
+      <div className='rounded-[10px] bg-white p-4  sm:p-6'>
+        <h3 className='mt-0.5 text-lg font-medium text-gray-900 '>{reviewBy}</h3>
 
         <p>{reviewComments}</p>
 
